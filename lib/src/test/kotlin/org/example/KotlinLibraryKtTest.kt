48edea840f73844527e210c07ba75f035a0a0b58
Finn Völkel <finn.volkel@gmail.com>
2024-09-17 10:35:04 +0200

    Call Kotlin and Java from repl

---
+++ lib/src/test/kotlin/org/example/KotlinLibraryKtTest.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class KotlinLibraryKtTest {
+
+    @Test
+    fun testMyFunction() {
+        assertEquals(myFunction(),"Hello, World!")
+    }
+}
